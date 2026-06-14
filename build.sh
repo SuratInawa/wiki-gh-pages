13a55691e2ba5936370eb8ee7d3b536fc5d0abde
Yohan Yukiya Sese-Cunetaㆍ사요한・謝雪矢 <techmagus@im.youronly.one>
2026-06-14 21:51:50 +0800

    Enhance build.sh for better output management
    Updated build script to improve rendering and organization of output files.
    
    Signed-off-by: Yohan Yukiya Sese-Cunetaㆍ사요한・謝雪矢 <techmagus@im.youronly.one>

--- build.sh
+++ build.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-#install tiddlywiki
+# Install tiddlywiki
 npm install tiddlywiki
 
-#unpack single file wiki into wiki folder
+# Prepare dist folder so index.html and static/ live side by side
+mkdir -p dist/static
+
+# Copy the live wiki to dist root so it is served at /
+cp index.html dist/index.html
+
+# Unpack single file wiki into wiki folder
 node node_modules/tiddlywiki/tiddlywiki.js --load index.html --savewikifolder ./wiki
 
-#render each non-system tiddler as a static HTML file
-node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./ --render '[!is[system]]' '[encodeuricomponent[]addprefix[static/]addsuffix[.html]]'  'text/plain' '$:/core/templates/static.tiddler.html'
+# Render each non-system tiddler keeping percent signs safe for proper multi-script decoding
+node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./dist --render '[!is[system]]' '[split[:]join[-]split[/]join[-]split[\]join[-]split[ ]join[-]split[?]join[-]split[<]join[-]split[>]join[-]split[|]join[-]split[*]join[-]split["]join[-]split[#]join[-]addprefix[static/]addsuffix[.html]]' 'text/plain' '$:/core/templates/static.tiddler.html'
 
-#render the css as an external file
-node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./ --render '$:/core/templates/static.template.css' 'static/static.css'  'text/plain'
+# Render the css as an external file
+node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./dist --render '$:/core/templates/static.template.css' 'static/static.css' 'text/plain'
 
-#render the index page
-node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./ --render '$:/core/templates/static.template.html' 'static/index.html' 'text/plain'
+# Render the index page using standard TiddlyWiki rendering
+node node_modules/tiddlywiki/tiddlywiki.js ./wiki --output ./dist --render '$:/core/templates/static.template.html' 'static/index.html' 'text/plain'
 
-#remove the wiki folder
-# rm -fr wiki
+# Run our updated post-processing code to handle banners and fix links
+node post-process.js
+
+# Remove the wiki folder
+rm -rf ./wiki
