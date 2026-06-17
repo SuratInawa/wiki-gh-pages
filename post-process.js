0a99cb141fa5cb776dc38979779236c3d82373e6
Yohan Yukiya Sese-Cunetaㆍ사요한・謝雪矢 <techmagus@im.youronly.one>
2026-06-17 21:11:50 +0800

    Fix sitemap XML generation and save to dist folder
    Signed-off-by: Yohan Yukiya Sese-Cunetaㆍ사요한・謝雪矢 <techmagus@im.youronly.one>

--- post-process.js
+++ post-process.js
@@ -127,7 +127,7 @@ if (fs.existsSync(rootIndex)) {
 // REQUIREMENT B: Compile and save sitemap.xml to the dist folder root
 const sitemapPath = path.join(__dirname, "dist", "sitemap.xml");
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://sitemaps.org">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${sitemapEntries.join("\n")}
 </urlset>`;
 
